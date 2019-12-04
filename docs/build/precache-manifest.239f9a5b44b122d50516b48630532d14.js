968accf0fe491ecba7a746049612897a8310a88a
saramarin23 <saramarin.econ@gmail.com>
2019-12-04 22:59:29 +0100

    change package json

---
+++ docs/build/precache-manifest.239f9a5b44b122d50516b48630532d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f30348bd6fbcce8f4496b747de05760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "261ee114cff75ecb42f7",
+    "url": "/static/css/main.ba6753ed.chunk.css"
+  },
+  {
+    "revision": "65491b8a4fe37687c1e0",
+    "url": "/static/js/2.222aad70.chunk.js"
+  },
+  {
+    "revision": "261ee114cff75ecb42f7",
+    "url": "/static/js/main.e11b8c5c.chunk.js"
+  },
+  {
+    "revision": "7b6e001f36bc8d5494a1",
+    "url": "/static/js/runtime-main.ea15b604.js"
+  },
+  {
+    "revision": "48dd8e7da8a472424f6170effad9635b",
+    "url": "/static/media/spidertocat.48dd8e7d.png"
+  }
+]);
